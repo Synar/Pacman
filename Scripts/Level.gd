abac88fcbe0ac4f857d2bde31f99f0a7882a8a6b
Younes Khoudli <younes.khoudli@epita.fr>
2020-06-12 01:17:02 +0200

    level: use the tilemap data to instanciate objects

---
+++ Scripts/Level.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var tilemap
+var pacmanScene = load("res://Scenes/pacman.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    tilemap = get_node("TileMap")
+    for pos in tilemap.get_used_cells_by_id(1):
+        var atlasPos = tilemap.get_cell_autotile_coord(pos.x, pos.y)
+        if atlasPos == Vector2(2, 0):
+            var pacman = pacmanScene.instance()
+            add_child(pacman)
+            pacman.speed = 50
+            pacman.position = tilemap.map_to_world(pos) + tilemap.position
+            tilemap.set_cell(pos.x, pos.y, 1, false, false, false, Vector2(0, 0))
+        if atlasPos == Vector2(1, 0): # dot
+            tilemap.set_cell(pos.x, pos.y, 1, false, false, false, Vector2(0, 0))
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
