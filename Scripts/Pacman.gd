f50dd0d611292363228522fc9d5394e4cd14dbd1
Younes Khoudli <younes.khoudli@epita.fr>
2020-06-11 04:47:46 +0200

    pacman: it moves!

--- Scripts/Pacman.gd
+++ Scripts/Pacman.gd
@@ -10,7 +10,24 @@ extends KinematicBody2D
 func _ready():
     pass # Replace with function body.
 
+export(float) var speed = 0
+var current_dir = Vector2(0, 0)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _process(delta):
+
+    if Input.is_action_pressed("move_left"):
+        current_dir = Vector2(-1, 0)
+
+    if Input.is_action_pressed("move_right"):
+        current_dir = Vector2(1, 0)
+
+    if Input.is_action_pressed("move_up"):
+        current_dir = Vector2(0, -1)
+
+
+    if Input.is_action_pressed("move_down"):
+        current_dir = Vector2(0, 1)
+
+    position += current_dir * speed * delta
