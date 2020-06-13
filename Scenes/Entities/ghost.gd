c8f7f9662a5682d4d36486485a2172df873b64b5
Synar <hakim.khoudli@polytechnique.org>
2020-06-13 15:29:01 +0200

    fixed frightened mode

--- Scenes/Entities/ghost.gd
+++ Scenes/Entities/ghost.gd
@@ -3,7 +3,7 @@ extends "res://Scripts/entity.gd"
 
 var target_pos = Vector2(0,0)
 var not_snapped = true
-var mode = "scatter" #"chase" "dead" "frightened" "waiting" #"init"?
+var mode = "scatter" #"chase" "dead" "frightened" "lockedIn" #"init"?
 var reverse_upon_leaving = [false,Vector2(0,0)]
 var scatter_timer = 0
 
@@ -21,8 +21,12 @@ var frame_count_post_turn = 0
 func update_mode(delta):
     if mode == "scatter" or mode == "frightened":
         scatter_timer+=delta
-    if fmod(scatter_timer,20.0)>=10.0:
+
+    if fmod(scatter_timer,16.0)>=8.0:
+        scatter_timer=0
+        mode = "frightened" if mode == "scatter" else "scatter"
         reverse_upon_leaving = [true, adjust_pos(position)]
+        print(mode)
 
 func pick_wanted_dir(delta):
     if not_snapped:
@@ -39,8 +43,6 @@ func pick_wanted_dir(delta):
         wanted_dir = - current_dir
         reverse_upon_leaving = [false,Vector2(0,0)]
 
-    elif mode == "frightened":
-        wanted_dir = [Vector2(-1, 0),Vector2(1, 0),Vector2(0, -1),Vector2(0, 1)][randi()%4]
 
     else :
         var potentialDir = [Vector2(0, 1),Vector2(-1, 0),Vector2(0, -1),Vector2(1, 0)]
@@ -48,11 +50,13 @@ func pick_wanted_dir(delta):
         for dir in potentialDir.duplicate():
             if tile_is_wall(position + 16*dir):
                 potentialDir.erase(dir)
-
-        wanted_dir = potentialDir[0]
-        for dir in potentialDir:
-            if (position+dir).distance_to(target_pos) < (position+wanted_dir).distance_to(target_pos):
-                wanted_dir = dir
+        if mode == "frightened" and potentialDir.size() > 1:
+            wanted_dir = potentialDir[randi()%potentialDir.size()]
+        else :
+            wanted_dir = potentialDir[0]
+            for dir in potentialDir:
+                if (position+dir).distance_to(target_pos) < (position+wanted_dir).distance_to(target_pos):
+                    wanted_dir = dir
 
     if wanted_dir != current_dir:
         frame_count_post_turn += 1
