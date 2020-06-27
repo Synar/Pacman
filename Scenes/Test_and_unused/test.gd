756bcaf9866bae9fd1815998af27b06190237865
Synar <hakim.khoudli@polytechnique.org>
2020-06-27 17:10:18 +0200

    added test scene and script

---
+++ Scenes/Test_and_unused/test.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    for i in 4:
+        print (i)
+    print(Color8(250,185,176,255))
+    print(Color8(33,33,255,255))
+    print(Color8(193,11,37,255))
+    print(Color8(218,222,242,255))
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
