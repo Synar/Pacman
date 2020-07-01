6ad077d3759ff9b78033450fa3faa226a55f2da9
Synar <hakim.khoudli@polytechnique.org>
2020-07-01 20:28:04 +0200

    Added first intermission with animation and sound

---
+++ Scenes/intermission1.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    OS.set_window_size(Vector2(520, 610))
+    #OS.set_window_position(screen_size*0.5 - window_size*0.5)
+    VisualServer.set_default_clear_color(000000)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
