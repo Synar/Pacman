648ce09f9dfeef1c4e02d04c37736a6b5348c60e
Synar <hakim.khoudli@polytechnique.org>
2020-07-02 01:32:59 +0200

    intermission music now plys exactly twice

---
+++ Scenes/AudioStreamPlayer.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var loop_count = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    if !self.playing:
+        if loop_count < 2:
+            loop_count += 1
+            self.play()
