b366676d447db7568998d3ab5c3e31c7162c784c
Synar <hakim.khoudli@polytechnique.org>
2020-07-16 23:52:18 +0200

    added continue option to the game over screen

--- Scripts/GUI/GameOver.gd
+++ Scripts/GUI/GameOver.gd
@@ -2,13 +2,16 @@ extends CanvasLayer
 
 
 func _ready():
-    OS.set_window_size(Vector2(640, 480))
+    #OS.set_window_size(Vector2(640, 480))
     VisualServer.set_default_clear_color(000000)
     pass # Replace with function body.
 
 
 func _input(event):
-    if event is InputEventKey:
+    if event.is_action_pressed("insert_coin"):
+            GlobalPlayer.score_increase(-1000)
+            get_tree().change_scene("res://Scenes/Level1.tscn")
+    elif event is InputEventKey:
         if event.pressed:
             get_tree().change_scene("res://Scenes/GUI/TitleScreen.tscn")
 
